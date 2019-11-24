7bc4e78845103964da2cf3521699423ce477cb31
Virendra <rajputvirendra361@gmail.com>
2019-11-25 04:51:33 +0530

    try Api

---
+++ buildfib.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+def fibonnacci (n):
+    if n < 1:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonnacci(n-1) + fibonnacci(n-2)
+
+@app.route("/")
+def send_number():
+    try:
+        return str(fibonnacci(int(request.args['n'])))
+    except ValueError:
+        return "Plese use proper integer"
